11e2b5b914f82d6a9a93d63454bcf9a88dffb539
MaKxex <70351676+MaKxex@users.noreply.github.com>
2020-09-06 19:51:01 +0300

    Add files via upload

---
+++ PixelHunter.py
@@ -0,0 +1,33 @@
+'''
+
+from tkinter import *
+root = Tk()
+def pixel
+
+b.bind('<Button-1>', pixel)
+b.pack
+
+
+root.mainloop()
+
+'''
+
+from tkinter import *
+from PIL import ImageTk, Image
+root = Tk()
+
+start_pos = None
+img = ImageTk.PhotoImage(Image.open("asd.png"))
+panel = Label(root, image = img)
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+def start_mouse_press(event):
+    global start_pos
+    start_pos = (event.x, event.y)
+
+def stop_mouse_press(event):
+    print(f"Start Pos {start_pos}\nEnd Pos {event.x, event.y}")
+
+
+root.bind("<ButtonPress-1>", start_mouse_press)
+root.bind("<ButtonRelease-1>", stop_mouse_press)
+mainloop()
