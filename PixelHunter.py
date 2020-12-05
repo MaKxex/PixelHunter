c583310227e0fb2b170ee8dfcb64f6e63ac7c5e4
MaKxex <70351676+MaKxex@users.noreply.github.com>
2020-12-04 18:34:24 -0800

    Add files via upload

--- PixelHunter.py
+++ PixelHunter.py
@@ -1,19 +1,18 @@
-from tkinter import *
-from PIL import ImageTk, Image
-root = Tk()
+import time
+import os, sys
+try:
+    import pyautogui
+except Exception as e:
+    print(e)
+    os.system('pip install pyautogui')
+    sys.exit("Restart the script.")
 
-start_pos = None
-img = ImageTk.PhotoImage(Image.open("asd.png"))
-panel = Label(root, image = img)
-panel.pack(side = "bottom", fill = "both", expand = "yes")
-def start_mouse_press(event):
-    global start_pos
-    start_pos = (event.x, event.y)
-
-def stop_mouse_press(event):
-    print(f"Start Pos {start_pos}\nEnd Pos {event.x, event.y}")
-
-
-root.bind("<ButtonPress-1>", start_mouse_press)
-root.bind("<ButtonRelease-1>", stop_mouse_press)
-mainloop()
+n = input("Сколько раз вывести координаты?")
+if n == 0:
+    print("Больше нуля")
+i = 0
+time.sleep(2)
+while i < int(n):
+    print(str(i) + str(pyautogui.position()))
+    time.sleep(2)
+    i +=1
